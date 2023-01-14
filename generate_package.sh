6dc119ffb43a4f6bdacaee58af1362c22856c849
Claas Beyersdorf <claas.beyersdorf@iant.de>
2023-01-15 00:58:20 +0100

    changed the generate_package.sh script (now we use the openapitools/openapi-generator-cli docker container) added additional command using the swaggerapi/swagger-codegen-cli-v3 (commented out)

--- generate_package.sh
+++ generate_package.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
-#sudo docker run --rm -v ${PWD}/../:/local openapitools/openapi-generator-cli generate \
-sudo docker run --rm -v ${PWD}/../:/local swaggerapi/swagger-codegen-cli generate \
+# NEW PHP 7.4 build with openapitools/openapi-generator-cli (should not need the null fix)
+sudo docker run --rm -v ${PWD}:/local openapitools/openapi-generator-cli generate \
 	-i https://api.easybill.de/rest/v1/swagger.json \
-	-l php \
-	-o /local/ \
+	-g php \
+	-o /local \
 	--git-user-id cbeyersdorf \
 	--git-repo-id easybill \
-	--invoker-package cbeyersdorf\\easybill \
-	-D packagePath=easybill,phpLegacySupport=false
+	--invoker-package "cbeyersdorf\easybill" \
+	--additional-properties=packageName=easybill
+
+# OLD PHP 5 build with swaggerapi/swagger-codegen-cli-v3 (needs the fix_null_problem.sh after generation)
+#sudo docker run --rm -v ${PWD}:/local swaggerapi/swagger-codegen-cli-v3 generate \
+#	-i https://api.easybill.de/rest/v1/swagger.json \
+#	-l php \
+#	-o /local/ \
+#	--git-user-id cbeyersdorf \
+#	--git-repo-id easybill \
+#	--invoker-package "cbeyersdorf\easybill" \
+#	-D packagePath=.
