34001d4c34d32ad76975cba57a8fa938d32278be
Claas Beyersdorf <claasfelix.beyersdorf@gmail.com>
2024-08-13 10:32:38 +0200

    changed ´generate_package.sh to run as unpriviledged user

--- generate_package.sh
+++ generate_package.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # NEW PHP 7.4 build with openapitools/openapi-generator-cli (needs the fix_null_problem.sh after generation)
-sudo docker run --rm -v ${PWD}:/local openapitools/openapi-generator-cli generate \
+docker run --user ${UID} --rm -v ${PWD}:/local openapitools/openapi-generator-cli generate \
 	-i https://api.easybill.de/rest/v1/swagger.json \
 	-g php \
 	-o /local \
@@ -10,7 +10,7 @@ sudo docker run --rm -v ${PWD}:/local openapitools/openapi-generator-cli generat
 	--additional-properties=packageName=easybill
 
 # OLD PHP 5 build with swaggerapi/swagger-codegen-cli-v3 (needs the fix_null_problem.sh after generation)
-#sudo docker run --rm -v ${PWD}:/local swaggerapi/swagger-codegen-cli-v3 generate \
+#docker run --rm -v ${PWD}:/local swaggerapi/swagger-codegen-cli-v3 generate \
 #	-i https://api.easybill.de/rest/v1/swagger.json \
 #	-l php \
 #	-o /local/ \
